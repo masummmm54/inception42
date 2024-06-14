ed1c95d5f09c612b3ae7a6856e0b527ceb964daf
Muhammet Kose <muhakose@1-D-3.42heilbronn.de>
2024-06-14 17:34:38 +0200

    masum

--- srcs/requirements/wordpress/tools/run.sh
+++ srcs/requirements/wordpress/tools/run.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 
+# Substitute environment variables in the PHP configuration
 sed -i 's|PHP_PORT|'${PHP_PORT}'|g' /etc/php/7.3/fpm/pool.d/www.conf
 
-if [ -f "/var/www/html/wordpress/wp-config.php" ]
-
-then
-  echo "Wordpress already confiured."
-
+# Download and configure WordPress if not already done
+if [ -f "$WP_PATH/wp-config.php" ]; then
+  echo "WordPress already configured."
 else
   wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
   chmod +x wp-cli.phar
@@ -16,7 +15,7 @@ else
   wp core install --path=$WP_PATH --url=$DOMAIN_NAME --title=$WP_TITLE --admin_user=$WP_USER --admin_password=$WP_PASSWORD --admin_email=$WP_EMAIL --skip-email --allow-root
   wp theme install teluro --path=$WP_PATH --activate --allow-root
   wp user create leon leon@le.on --role=author --path=$WP_PATH --user_pass=leon --allow-root
-
 fi
 
+# Start PHP-FPM
 /usr/sbin/php-fpm7.3 -F
