c41b83dbbed3c4bf23f33d891873682cefc404a4
Muhammet Kose <muhakose@1-D-3.42heilbronn.de>
2024-06-14 15:43:41 +0200

    masum

--- srcs/requirements/mariadb/tools/installation.sh
+++
@@ -1,6 +0,0 @@
-mysql -e "CREATE DATABASE IF NOT EXISTS $db_name ;"
-mysql -e "CREATE USER IF NOT EXISTS '$db_user'@'%' IDENTIFIED BY '$db_user_passwd' ;"
-mysql -e "GRANT ALL PRIVILEGES ON $db_name.* TO '$db_user'@'%' ";
-mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$rootpasswd' ;"
-mysql -e "FLUSH PRIVILEGES";
-mysqladmin -u root -p$rootpasswd shutdown
--- srcs/requirements/wordpress/conf/conf.php
+++
@@ -1,5 +0,0 @@
-define( 'WP_REDIS_HOST', 'redis');
-define( 'WP_REDIS_PORT', 6379 );
-define( 'WP_CACHE', true );
-
-require_once ABSPATH . 'wp-settings.php';
