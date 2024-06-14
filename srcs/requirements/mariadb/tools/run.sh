a5e59f23fda74d231b8e681bb708affc852da947
Muhammet Kose <muhakose@1-D-3.42heilbronn.de>
2024-06-14 17:58:29 +0200

    masum

--- srcs/requirements/mariadb/tools/run.sh
+++ srcs/requirements/mariadb/tools/run.sh
@@ -7,16 +7,12 @@ sed -i 's|MYSQL_ROOT_PASSWORD|'${MYSQL_ROOT_PASSWORD}'|g' /tmp/init.sql
 sed -i 's|MYSQL_PORT|'${MYSQL_PORT}'|g' /etc/mysql/my.cnf
 sed -i 's|MYSQL_ADDRESS|'${MYSQL_ADDRESS}'|g' /etc/mysql/my.cnf
 
-if [ -d "/var/lib/mysql/$MYSQL_DATABASE" ]
-
-then
+if [ -d "/var/lib/mysql/$MYSQL_DATABASE" ]; then
   echo "Database already exists."
   mysqld_safe
-
 else
   mysql_install_db
   mysqld --init-file="/tmp/init.sql"
-
 fi
 
 exec "$@"
