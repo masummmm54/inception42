a01f44a2b47d5516235fc409b0cbfd86fdbae0e8
Muhammet Kose <muhakose@1-D-3.42heilbronn.de>
2024-06-15 18:46:58 +0200

    nginx-Docker-filels

--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -1,57 +1,50 @@
 #!/bin/bash
 
-
-
-# create directory to use in nginx container later and also to setup the wordpress conf
-mkdir /var/www/
-mkdir /var/www/html
+# Create directories for WordPress
+mkdir -p /var/www/html
 
 cd /var/www/html
 
-
+# Clean up any existing files
 rm -rf *
 
-curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar 
-
-chmod +x wp-cli.phar 
-
+# Download wp-cli
+curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+chmod +x wp-cli.phar
 mv wp-cli.phar /usr/local/bin/wp
 
-
+# Download and configure WordPress
 wp core download --allow-root
-
-mv /var/www/html/wp-config-sample.php /var/www/html/wp-config.php
-
 mv /wp-config.php /var/www/html/wp-config.php
 
+# Update wp-config.php with environment variables
+sed -i -r "s/db1/$DB_NAME/1" wp-config.php
+sed -i -r "s/user/$DB_USER/1" wp-config.php
+sed -i -r "s/pwd/$DB_PASSWORD/1" wp-config.php
 
-sed -i -r "s/db1/$db_name/1"   wp-config.php
-sed -i -r "s/user/$db_user/1"  wp-config.php
-sed -i -r "s/pwd/$db_pwd/1"    wp-config.php
+# Install WordPress
+wp core install --url=$DOMAIN_NAME --title=$WP_TITLE --admin_user=$WP_ADMIN_USR --admin_password=$WP_ADMIN_PWD --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root
 
-wp core install --url=$DOMAIN_NAME/ --title=$WP_TITLE --admin_user=$WP_ADMIN_USR --admin_password=$WP_ADMIN_PWD --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root
-
-
-
-
+# Create a new WordPress user
 wp user create $WP_USR $WP_EMAIL --role=author --user_pass=$WP_PWD --allow-root
 
-
+# Install and activate the Astra theme
 wp theme install astra --activate --allow-root
 
-
+# Install and activate the Redis cache plugin
 wp plugin install redis-cache --activate --allow-root
 
+# Update all plugins
 wp plugin update --all --allow-root
 
+# Update PHP-FPM configuration to listen on port 9000
+sed -i 's/listen = \/run\/php\/php7\/fpm\/php-fpm.sock/listen = 9000/g' /etc/php7/php-fpm.d/www.conf
 
- 
-sed -i 's/listen = \/run\/php\/php7.3-fpm.sock/listen = 9000/g' /etc/php/7.3/fpm/pool.d/www.conf
+# Create directory for PHP-FPM runtime
+mkdir -p /run/php
 
-mkdir /run/php
-
-
-
+# Enable Redis cache
 wp redis enable --allow-root
 
-/usr/sbin/php-fpm7.3 -F
+# Start PHP-FPM
+php-fpm7 -F
