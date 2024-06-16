07f5b0b7c50020a09515e69bfde8e464befede1d
Muhammet Kose <muhakose@1-D-3.42heilbronn.de>
2024-06-16 10:55:17 +0200

    fix

--- srcs/requirements/nginx/tools/script.sh
+++ srcs/requirements/nginx/tools/script.sh
@@ -8,8 +8,9 @@ if [ ! -f /etc/nginx/ssl/nginx-selfsigned.crt ]; then
     openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/nginx/ssl/nginx-selfsigned.key -out /etc/nginx/ssl/nginx-selfsigned.crt -subj "/CN=localhost"
 fi
 
-# Link the default config file
-ln -s /etc/nginx/sites-available/default /etc/nginx/sites-enabled/
+if [ ! -L /etc/nginx/sites-enabled/default ]; then
+    ln -s /etc/nginx/sites-available/default /etc/nginx/sites-enabled/
+fi
 
 # Start Nginx
 nginx -g 'daemon off;'
