72ba261700098a38b2ebd6d5860b3bb27934214a
Muhammet Kose <muhakose@1-D-3.42heilbronn.de>
2024-06-16 15:13:43 +0200

    fix

--- srcs/requirements/wordpress/tools/conf.sh
+++ srcs/requirements/wordpress/tools/conf.sh
@@ -40,7 +40,7 @@ wp core config --dbhost=mariadb:3306 --dbname="$MYSQL_DB" --dbuser="$MYSQL_USER"
 wp core install --url="$DOMAIN_NAME" --title="$WP_TITLE" --admin_user="$WP_ADMIN_N" --admin_password="$WP_ADMIN_P" --admin_email="$WP_ADMIN_E" --allow-root
 wp user create "$WP_U_NAME" "$WP_U_EMAIL" --user_pass="$WP_U_PASS" --role="$WP_U_ROLE" --allow-root
 
-sed -i '36 s@/rub/php/php7.4-fpm.sock@9000@' /etc/php/7.4/fpm/pool.d/www.conf
+sed -i '36 s@/run/php/php7.4-fpm.sock@9000@' /etc/php/7.4/fpm/pool.d/www.conf
 
 mkdir -p /run/php
 
